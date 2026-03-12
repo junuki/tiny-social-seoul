f6e512e99a97336303b3a9c22d2f47287736c27c
Tyhoon <umteeh@gmail.com>
2026-03-12 14:21:51 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tiny-social-seoul/',
+  base: '/',
   plugins: [react()]
 });
